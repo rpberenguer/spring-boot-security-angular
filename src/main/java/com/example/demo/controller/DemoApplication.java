d2e96073efdb3d2a9240c260edb8bed97c423f95
Raul <rpberenguer@gmail.com>
2018-09-11 16:26:51 +0200

    fixes

--- src/main/java/com/example/demo/controller/DemoApplication.java
+++ src/main/java/com/example/demo/controller/DemoApplication.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
